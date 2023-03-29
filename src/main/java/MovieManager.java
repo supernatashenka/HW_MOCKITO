20e4d826fd33da9e47d0997f3ca6dda8bccbb6dc
Netology QA <obgynkostina@gmail.com>
2023-03-29 14:04:26 +0300

    reformated code

--- src/main/java/MovieManager.java
+++ src/main/java/MovieManager.java
@@ -1,44 +1,47 @@
 public class MovieManager {
 
     private int limit;
-    public MovieManager(){
+
+    public MovieManager() {
         this.limit = 5;
 
     }
-    public MovieManager (int limit) {
+
+    public MovieManager(int limit) {
         this.limit = limit;
 
     }
 
     private String[] movies = new String[0];
-    public String addMovie (String movie) {
-        String []tmp = new String [movies.length + 1];
-        for (int i =0; i < movies.length; i++){
-            tmp [i] = movies [i];
+
+    public String addMovie(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
         }
 
-     tmp [tmp.length - 1] = movie;
+        tmp[tmp.length - 1] = movie;
         movies = tmp;
 
         return movie;
     }
 
-    public String [] findAllMovies() {
+    public String[] findAllMovies() {
         return movies;
     }
 
-    public String[] findLast(){
+    public String[] findLast() {
         int resultsCount;
-        if(movies.length < limit){
+        if (movies.length < limit) {
             resultsCount = movies.length;
 
-        } else{
+        } else {
             resultsCount = limit;
         }
-        String [] tmp = new String[resultsCount];
+        String[] tmp = new String[resultsCount];
         for (int i = 0; i < tmp.length; i++) {
-            tmp [i] = movies[movies.length-1-i];
-            
+            tmp[i] = movies[movies.length - 1 - i];
+
         }
         return tmp;
 
